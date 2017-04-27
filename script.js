ceaa7d7a26e581bb851523603f3fd8c1eb95cc58
guenthertaras <taras.guenther@bfr.bund.de>
2017-04-27 15:52:39 +0200

    CHanges in search function

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ require(['jQuery320'], function (jQuery320) {
     '<button class="accordion-control">Panel1</button>' +
     '<div class="accordion-panel">' +
     '<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>' +
+    '<table> <tr> <td>Inhalt</td> </tr> </table>'+
     '</div>' +
     '</li>' +
     '<li>' +
@@ -56,10 +57,11 @@ require(['jQuery320'], function (jQuery320) {
   var $search = $('#filter-search');      // Get the input element
   var cache = [];                         // Create an array called cache
 
-  $li.each(function () {                 
+  $li.each(function () {
+    console.log(getText(this));
     cache.push({                          // Add an object to the cache array
       element: this,                      // This image
-      text: (this.childNodes[0].outerText + this.childNodes[1].childNodes[0].innerText).trim().toLowerCase()// Its outerText (lowercase trimmed)
+      text: getText(this).trim().toLowerCase()// Its outerText (lowercase trimmed)
     });
   });
   function filter() {                     // Declare filter() function
@@ -78,6 +80,24 @@ require(['jQuery320'], function (jQuery320) {
     $search.on('input', filter);          // Use input event to call filter()
   } else {                                // Otherwise
     $search.on('keyup', filter);          // Use keyup event to call filter()
+  }
+
+  function getText(element) {
+    let text = '';
+
+    if (element.outerText) {
+      text += element.outerText.trim() + ' ';
+    }
+
+    if (element.innerText) {
+      text += element.innerText.trim() + ' ';
+    }
+
+    for (var child of element.childNodes) {
+      text += getText(child);
+    }
+
+    return text;
   }
 })();
 
